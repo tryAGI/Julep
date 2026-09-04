70800c2997b4d0029362f275fb2fe64e0132e597
Konstantin S. <havendv@gmail.com>
2026-09-04 06:44:15 +0400

    feat: Updated OpenAPI spec (#46)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Julep/Generated/Julep.JsonSerializerContext.g.cs
+++ src/libs/Julep/Generated/Julep.JsonSerializerContext.g.cs
@@ -2031,6 +2031,7 @@ namespace Julep
     {
         private static readonly global::System.Text.Json.Serialization.Metadata.IJsonTypeInfoResolver Resolver = new LazyChunkResolver();
 
+
         private static readonly global::System.Text.Json.JsonSerializerOptions DefaultOptions = CreateDefaultOptions();
 
         /// <summary>
@@ -2052,13 +2053,8 @@ namespace Julep
             return Resolver.GetTypeInfo(type, Options);
         }
 
-        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+         static void AddConverters(global::System.Text.Json.JsonSerializerOptions options)
         {
-            var options = new global::System.Text.Json.JsonSerializerOptions
-            {
-                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
-                TypeInfoResolver = Resolver,
-            };
             options.Converters.Add(new global::Julep.JsonConverters.ResponsesInputItemJsonConverter());
             options.Converters.Add(new global::Julep.JsonConverters.ResponsesItemReferenceJsonConverter());
             options.Converters.Add(new global::Julep.JsonConverters.ResponsesOutputItemJsonConverter());
@@ -2191,8 +2187,17 @@ namespace Julep
             options.Converters.Add(new global::Julep.JsonConverters.AnyOfJsonConverter<global::Julep.DocsVectorDocSearchRequest, global::Julep.DocsTextOnlyDocSearchRequest, global::Julep.DocsHybridDocSearchRequest>());
             options.Converters.Add(new global::Julep.JsonConverters.AnyOfJsonConverter<global::Julep.ChatChunkChatResponse, global::Julep.ChatMessageChatResponse>());
             options.Converters.Add(new global::Julep.JsonConverters.UnixTimestampJsonConverter());
-
             options.Converters.Add(new LazyEnumJsonConverterFactory());
+        }
+
+        private static global::System.Text.Json.JsonSerializerOptions CreateDefaultOptions()
+        {
+            var options = new global::System.Text.Json.JsonSerializerOptions
+            {
+                DefaultIgnoreCondition = global::System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull,
+                TypeInfoResolver = Resolver,
+            };
+            AddConverters(options);
 
             return options;
         }
